235aacf2129deb3d0ce37e70a9617877e780af98
Jeronimo Pellegrini <j_p@aleph0.info>
2019-04-05 20:31:42 -0300

    Add licence note to all files

--- carteiro.lisp
+++ carteiro.lisp
@@ -1,3 +1,23 @@
+#|
+  This file is a part of carteiro project.
+  Copyright (c) 2019 Edgard Bikelis (bikelis@gmail.com)
+
+  Author: Edgard Bikelis (bikelis@gmail.com)
+
+  Carteiro is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or (at
+  your option) any later version.
+
+  Carteiro is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Carteiro.  If not, see <https://www.gnu.org/licenses/>.
+|#
+
 (defpackage carteiro
   (:use :cl)
   (:export rastreio))
